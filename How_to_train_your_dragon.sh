b7ca2e01a6385aaea5f83760ea41fbe245af73de
Zhiqi Guo <guozhiqi14@gmail.com>
2017-12-12 19:53:59 -0500

    。。。

--- How_to_train_your_dragon.sh
+++ How_to_train_your_dragon.sh
@@ -7,5 +7,7 @@ pip install pytorch
 
 
 scp -r yz1349@prince.hpc.nyu.edu:/scratch/yz1349/DiscSentEmbed zg475@prince.hpc.nyu.edu:/scratch/zg475 
+sbatch train_joint.sh 
+squeue -u zg475
 
 transit server name: prince.hpc.nyu.edu
